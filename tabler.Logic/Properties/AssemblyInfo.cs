163ffa00a7794d0394a5b6b771bf4265f386651f
bux <github@jonathandavid.de>
2019-03-11 10:11:27 +0100

    Bump Version Number

--- tabler.Logic/Properties/AssemblyInfo.cs
+++ tabler.Logic/Properties/AssemblyInfo.cs
@@ -21,15 +21,12 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("651801fa-f7ae-4f9b-9a7c-2ddea5965440")]
 
-// Version information for an assembly consists of the following four values:
+// http://semver.org/
+// Version information for an assembly consists of the following values:
 //
 //      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.1")]
-[assembly: AssemblyFileVersion("0.9.1")]
+//      Minor Version 
+//      Patch
+
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
--- tabler/Properties/AssemblyInfo.cs
+++ tabler/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Minor Version 
 //      Patch
 
-[assembly: AssemblyVersion("0.9.1")]
-[assembly: AssemblyFileVersion("0.9.1")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
